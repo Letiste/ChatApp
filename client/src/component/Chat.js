798c053a6d3de9f1299f8c9dbc94af64bf03af2b
Léo Salé <leo.sale72@gmail.com>
2020-10-13 11:31:25 +0200

    setup router

---
+++ client/src/component/Chat.js
@@ -0,0 +1,8 @@
+import React from "react"
+
+export default function Chat() {
+
+  return (
+    <h1>Chat</h1>
+  )
+}
---
+++ client/src/component/Join.js
@@ -0,0 +1,8 @@
+import React from "react"
+
+export default function Join() {
+
+  return (
+    <h1>Join</h1>
+  )
+}
