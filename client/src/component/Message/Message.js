9fab49f434a850ab6b7159921730d9e6ace6b943
Léo Salé <leo.sale72@gmail.com>
2020-10-14 16:48:17 +0200

    add Messages component

---
+++ client/src/component/Message/Message.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+import "./Message.css";
+
+import ReactEmoji from "react-emoji"
+
+export default function Message({ message: { user, text }, name }) {
+  let isSentByCurrentUser = false;
+
+  const trimmedName = name.trim().toLowerCase();
+
+  if (user === trimmedName) {
+    isSentByCurrentUser = true;
+  }
+
+  return isSentByCurrentUser ? (
+    <div className="messageContainer justifyEnd">
+      <p className="senText pr-10">{trimmedName}</p>
+      <div className="messageBox backgroundBlue">
+        <p className="messageText colorWhite">{ReactEmoji.emojify(text)}</p>
+      </div>
+    </div>
+  ) : (
+    <div className="messageContainer justifyStart">
+      <div className="messageBox backgroundLight">
+        <p className="messageText colorDark">{ReactEmoji.emojify(text)}</p>
+      </div>
+      <p className="senText">{user}</p>
+    </div>
+  );
+}
